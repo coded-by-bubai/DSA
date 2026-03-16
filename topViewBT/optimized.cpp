f354939fda0a7bf91ebe500fc265de8fcac660ea
Bubai Das <bubai801620@gmail.com>
2026-03-16 22:18:49 +0530

    Graph: top view of tree

---
+++ topViewBT/optimized.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<queue>
+#include<climits>
+
+using namespace std;
+class Node
+{ 
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+vector<int> topView(Node* root){
+    if (!root) return {};
+    unordered_map<int, int> topViewNodes;
+    queue<pair<Node*, int>> q;
+    q.push({root, 0});
+    int mn = INT_MAX;
+
+    while (!q.empty())
+    {
+        auto currNode = q.front();
+        Node* node = currNode.first;
+        int hd = currNode.second;
+        q.pop();
+        mn = min(mn, hd);
+
+        if (topViewNodes.find(hd) == topViewNodes.end())
+        {
+            topViewNodes[hd] = node->data;
+        }
+        if (node->left)
+        {
+            q.push({node->left, hd-1});
+        }
+        if (node->right)
+        {
+            q.push({node->right, hd+1});
+        }
+        
+    }
+    vector<int> result(topViewNodes.size());
+    
+    for(auto it = topViewNodes.begin(); it != topViewNodes.end(); it++){
+        result[it->first - mn] = it->second;
+    }
+    return result;
+}
+
+int main(){
+    // Create a sample binary tree
+    //     10
+    //    /  \
+    //   20   30
+    //  / \   / \
+    // 40  60 90  100
+
+    Node* root = new Node(10);
+    root->left = new Node(20);       
+    root->right = new Node(30);            
+    root->left->left = new Node(40);     
+    root->left->right = new Node(60);   
+    root->right->left = new Node(90);   
+    root->right->right = new Node(100); 
+
+    vector<int> result = topView(root);
+    for (int i : result) {
+        cout << i << " ";
+    }
+    return 0;
+}
