46160a2ef6f6cc71fdcc1943d51935a76e908a53
Bubai Das <bubai801620@gmail.com>
2026-07-27 23:20:17 +0530

    Linked list palindrome

--- Palindrome/README.md
+++ Palindrome/README.md
@@ -7,3 +7,11 @@ Output: true
 Explanation: 121 reads as 121 from left to right and from right to left.
 
 ---
+
+# 2. Palindrome Linked List
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+example:
+Input: head = [1,2,2,1]
+Output: true
+
+---
---
+++ Palindrome/linked_list_palindrome.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+    Node(int d){
+        data = d;
+        next = nullptr;
+    };
+};
+
+Node* reverse(Node* head){
+    Node* prev = nullptr;
+    Node* curr = head;
+    Node* next;
+
+    while(curr){
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+bool isIdentical(Node* n1, Node* n2){
+    for(;n1 && n2; n1 = n1->next, n2 = n2->next){
+        if(n1->data != n2->data)
+            return 0;
+    }
+    return 1;
+}
+
+bool isPalindrome(Node* head){
+    if(!head || !head->next)
+        return true;
+    
+    Node* slow = head;
+    Node* fast = head;
+    while(fast->next && fast->next->next){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    Node* head2 = reverse(slow->next);
+    slow->next = nullptr;
+
+    bool ret = isIdentical(head, head2);
+
+    head2 = reverse(head2);
+    slow->next = head2;
+    return ret;
+}
+
+int main(){
+    Node head(1);
+    head.next = new Node(2);
+    head.next->next = new Node(3);
+    head.next->next->next = new Node(2);
+    head.next->next->next->next = new Node(1);
+
+    bool result = isPalindrome(&head);
+    cout << (result ? "True": "False");
+    return 0;
+}
