f1fcc8f0ef7753c2f4a529146c858302f08b0e7e
Bubai Das <bubai801620@gmail.com>
2026-08-10 23:00:52 +0530

    search in a rotated sorted array

---
+++ search in rotated sorted array/optimal.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int search(vector<int>& nums, int target) {
+    int st = 0, end = nums.size() - 1;
+
+    while (st <= end) {
+        int mid = st + (end - st) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+
+        if (nums[st] <= nums[mid]) {
+            if (nums[st] <= target && target <= nums[mid]) {
+                end = mid - 1;
+            } else {
+                st = mid + 1;
+            }
+        } else {
+            if (nums[mid] <= target && target <= nums[end]) {
+                st = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> arr = {7,8,0,1,2,3,4,5,6};
+    cout << search(arr, 2);
+}
